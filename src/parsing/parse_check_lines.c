17a7a6041eb321c9ecf96456d23462db1ce96228
lmattern <ludmattern@gmail.com>
2024-05-20 16:06:56 +0200

    end of parsing

---
+++ src/parsing/parse_check_lines.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_check_lines.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 14:00:32 by lmattern          #+#    #+#             */
+/*   Updated: 2024/05/20 09:10:52 by lmattern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3D.h"
+
+static int	check_line_char(char *line)
+{
+	int			i;
+	static bool	player = false;
+
+	i = 0;
+	if (!line)
+		return (SUCCESS);
+	while (line[i])
+	{
+		if (line[i] != ' ' && line[i] != '1' && line[i] != '0'
+			&& line[i] != 'N' && line[i] != 'S' && line[i] != 'E'
+			&& line[i] != 'W' && line[i] != '\n')
+		{
+			ft_eprintf(ERR"Invalid character in map : %c\n", line[i]);
+			return (ERROR);
+		}
+		if (!player && (line[i] == 'N' || line[i] == 'S'
+				|| line[i] == 'E' || line[i] == 'W'))
+			player = true;
+		else if (player && (line[i] == 'N' || line[i] == 'S'
+				|| line[i] == 'E' || line[i] == 'W'))
+			return (ft_eprintf(ERR"Multiple players\n"), ERROR);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+static int	last_checks(t_cub *cub, bool player, char *line)
+{
+	ft_free(line);
+	if (cub->map.height < 3)
+		return (ft_eprintf(ERR"Map is too small\n"), ERROR);
+	if (!player)
+		return (ft_eprintf(ERR"Player is missing\n"), ERROR);
+	return (SUCCESS);
+}
+
+int	extract_map_lines(t_cub *cub, char **line, char ***lines)
+{
+	size_t	i;
+	bool	player;
+
+	i = 0;
+	*lines = NULL;
+	player = false;
+	while (1)
+	{
+		if (!check_line_char(*line))
+			return (ERROR);
+		*lines = ft_realloc(*lines, i * sizeof(char *), \
+		(i + 2) * sizeof(char *));
+		if (!*lines)
+			return (ft_eprintf(ERR ALLOC_ERR" for map\n"), ERROR);
+		(*lines)[i++] = *line;
+		cub->map.height++;
+		if (ft_strchr(*line, 'N') || ft_strchr(*line, 'S')
+			|| ft_strchr(*line, 'E') || ft_strchr(*line, 'W'))
+			player = true;
+		*line = get_next_line(cub->filefd);
+		if (!*line || !**line || **line == '\n')
+			break ;
+	}
+	return (last_checks(cub, player, *line));
+}
+
+int	check_end_of_file(int fd, char **line)
+{
+	while (1)
+	{
+		*line = get_next_line(fd);
+		if (*line && **line != '\n')
+			return (ft_eprintf(ERR"Map is not well formatted\n"), ERROR);
+		if (!*line || !**line)
+			break ;
+		free(*line);
+	}
+	return (SUCCESS);
+}
+
+int	skip_empty_lines(int fd, char **line)
+{
+	*line = NULL;
+	while (1)
+	{
+		*line = get_next_line(fd);
+		if (!line || !*line)
+		{
+			ft_eprintf(ERR"Map is missing\n");
+			return (get_next_line(-1), ERROR);
+		}
+		if ((*line)[0] != '\n')
+			break ;
+		free(*line);
+	}
+	return (SUCCESS);
+}
---
+++ src/parsing/parse_check_map.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_check_map.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 14:00:32 by lmattern          #+#    #+#             */
+/*   Updated: 2024/05/20 16:02:26 by lmattern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/cub3D.h"
+
+static int	inside_loop(t_pos *stack, int top, t_map *map, int heading[4][2])
+{
+	size_t	i;
+	size_t	nr;
+	size_t	nc;
+	int		row;
+	int		col;
+
+	i = -1;
+	row = stack[top].row;
+	col = stack[top].col;
+	while (++i < 4)
+	{
+		nr = row + heading[i][0];
+		nc = col + heading[i][1];
+		if (nr < 0 || nr >= map->height || nc < 0 || nc >= map->width)
+			return (ERROR);
+		else if (map->grid[nr][nc] == '0')
+		{
+			stack[top].row = nr;
+			stack[top].col = nc;
+			top++;
+		}
+		else if (map->grid[nr][nc] != '1' && map->grid[nr][nc] != '2')
+			return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+static void	init_heading_map(int heading[4][2])
+{
+	heading[0][0] = -1;
+	heading[0][1] = 0;
+	heading[1][0] = 1;
+	heading[1][1] = 0;
+	heading[2][0] = 0;
+	heading[2][1] = -1;
+	heading[3][0] = 0;
+	heading[3][1] = 1;
+}
+
+static int	flood_fill(t_map *map, int start_row, int start_col)
+{
+	int		top;
+	int		heading[4][2];
+	t_pos	*stack;
+
+	top = 0;
+	init_heading_map(heading);
+	stack = (t_pos *)ft_calloc(map->height * map->width, sizeof(t_pos));
+	if (!stack)
+		return (free(stack), ft_eprintf(ERR ALLOC_ERR" for stack\n"), ERROR);
+	stack[top].row = start_row;
+	stack[top].col = start_col;
+	top++;
+	while (top > 0)
+	{
+		top--;
+		if (map->grid[stack[top].row][stack[top].col] != '0')
+			continue ;
+		map->grid[stack[top].row][stack[top].col] = '2';
+		if (!inside_loop(stack, top, map, heading))
+			return (free(stack), ERROR);
+	}
+	return (free(stack), SUCCESS);
+}
+
+void	print_map(t_map *map)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (i < map->height)
+	{
+		j = 0;
+		while (j < map->width)
+		{
+			ft_printf("%c", map->grid[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+}
+
+int	check_map_validity(t_map *map)
+{
+	size_t	row;
+	size_t	col;
+
+	row = -1;
+	while (++row < map->height)
+	{
+		col = -1;
+		while (++col < map->width)
+		{
+			if (map->grid[row][col] != '0')
+				continue ;
+			if (!flood_fill(map, row, col))
+			{
+				ft_printf(ERR"Map is not surrounded by walls\n");
+				ft_free_double_int_array(map->grid, map->height);
+				return (ERROR);
+			}
+		}
+	}
+	return (SUCCESS);
+}
