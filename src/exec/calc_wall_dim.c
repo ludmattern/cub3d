309d633c9477e1f9af9c28d9942349f5ec672cd5
ludmattern <github.rfgrg@passmail.net>
2024-05-21 12:54:34 +0200

    norme and patch the incorrect behaviour when color was 0,0,0

--- src/exec/calc_wall_dim.c
+++ src/exec/calc_wall_dim.c
@@ -6,7 +6,7 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:03:18 by fprevot           #+#    #+#             */
-/*   Updated: 2024/05/21 11:28:33 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/05/21 12:41:39 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,28 @@
 this function calculates the distance between the player and the wall hit by 
 the ray.
 */
-void calc_wall_distance(t_scene_ctx *ctx, t_ray *rc)
+void	calc_wall_distance(t_scene_ctx *ctx, t_ray *rc)
 {
-    if (ctx->side == 0)
-        ctx->wall_dist = (ctx->map_x - rc->pos_x + (1 - ctx->step_x) / 2) / ctx->ray_dir_x;
-    else
-        ctx->wall_dist = (ctx->map_y - rc->pos_y + (1 - ctx->step_y) / 2) / ctx->ray_dir_y;
-
-    ctx->line_height = (int)(rc->win_height / ctx->wall_dist);
+	if (ctx->side == 0)
+		ctx->wall_dist = (ctx->map_x - rc->pos_x + (1 - ctx->step_x) / 2)
+			/ ctx->ray_dir_x;
+	else
+		ctx->wall_dist = (ctx->map_y - rc->pos_y + (1 - ctx->step_y) / 2)
+			/ ctx->ray_dir_y;
+	ctx->line_height = (int)(rc->win_height / ctx->wall_dist);
 }
 
 /*
 this function calculates the positions where the wall will be drawn.
 */
-void calc_draw_positions(t_scene_ctx *ctx, t_ray *rc)
+void	calc_draw_positions(t_scene_ctx *ctx, t_ray *rc)
 {
-    ctx->draw_start = -ctx->line_height / 2 + rc->win_height / 2; // calc debut de ligne
-    if (ctx->draw_start < 0)
-        ctx->draw_start = 0;
-    ctx->draw_end = ctx->line_height / 2 + rc->win_height / 2; // calc fin de ligne
-    if (ctx->draw_end >= rc->win_height)
-        ctx->draw_end = rc->win_height - 1;
+	ctx->draw_start = -ctx->line_height / 2 + rc->win_height / 2;
+	if (ctx->draw_start < 0)
+		ctx->draw_start = 0;
+	ctx->draw_end = ctx->line_height / 2 + rc->win_height / 2;
+	if (ctx->draw_end >= rc->win_height)
+		ctx->draw_end = rc->win_height - 1;
 }
 
 /*
@@ -57,11 +58,11 @@ this function calculates the x position in the texture.
 */
 void	calc_texture_x(t_scene_ctx *ctx, t_ray *rc)
 {
-	ctx->texture_x = (int)(ctx->wall_x * (double)rc->texture[ctx->texture_num].width);
+	ctx->text_x = (int)(ctx->wall_x * (double)rc->texture[ctx->text_num].width);
 	if (ctx->side == 0 && ctx->ray_dir_x > 0)
-		ctx->texture_x = rc->texture[ctx->texture_num].width - ctx->texture_x - 1;
+		ctx->text_x = rc->texture[ctx->text_num].width - ctx->text_x - 1;
 	if (ctx->side == 1 && ctx->ray_dir_y < 0)
-		ctx->texture_x = rc->texture[ctx->texture_num].width - ctx->texture_x - 1;
+		ctx->text_x = rc->texture[ctx->text_num].width - ctx->text_x - 1;
 }
 
 /*
